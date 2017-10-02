18e4a8bc7089ee39374ad0e4e86d18c9e572ce3a
Thomas Mbise & Simon Brady <pair+heretoinfinity.sbrady11@devbootcamp.com>
2017-10-02 16:24:02 -0400

    Remove unused button in submitButton component

--- src/components/submitButton.js
+++ src/components/submitButton.js
@@ -5,7 +5,6 @@ class SubmitButton extends React.Component {
   render () {
     return (
       <div>
-      <button onClick={ this.props.handleClick }> SUBMIT </button>
       <button
         onClick={ this.props.handleRspecCodeSubmit }
         className='send'>
