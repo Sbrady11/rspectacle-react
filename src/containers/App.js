8d325562fde25db3429523b54ef5fd366e14859a
Sang Wook Bok & Simon Brady <pair+sambok.sbrady11@devbootcamp.com>
2017-10-02 11:53:30 -0400

    Clean up the code

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,8 +12,7 @@ class App extends Component {
     this.state = {
       rspecCode: '',
       rubyCode: '',
-      testResult: '',
-      chatLogs: [] // Not gonna use it
+      testResult: ''
     };
   }
 
@@ -50,23 +49,6 @@ class App extends Component {
       }
     });
   }
-  renderChatLog() {
-    return this.state.chatLogs.map((el) => {
-      return (
-        <li key={`chat_${el.id}`}>
-          <span className='chat-message'>{ el.content }</span>
-          <span className='chat-created-at'>{ el.created_at }</span>
-        </li>
-      );
-    });
-  }
-
-  // handleSubmitButtonClick (string) {
-  //   this.setState({
-  //     inputDisplay: string
-  //   });
-  // }
-
   render() {
     return (
       <div className="App">
@@ -79,83 +61,11 @@ class App extends Component {
           <div className="output" style={{ backgroundColor: "black", padding: "1em", margin: "1em 0em" }}>
             <ConsoleOutput />
           </div>
-
-        </div> { /* end of className='playground' */ }
-        <div className='playground'>
-          <div className='code-display'>
-            <h1>Code</h1>
-            { /* <SubmitButton handleClick={ this.handleSubmitButtonClick } /> */}
-            <ul className='chat-logs'>
-              { this.renderChatLog() }
-            </ul>
-            <input
-              onKeyPress={ (e) => this.handleChatInputKeyPress(e) }
-              value={ this.state.currentChatMessage }
-              onChange={ (e) => this.updateCurrentChatMessage(e) }
-              type='text'
-              placeholder='Enter your code...'
-              className='chat-input' />
-            <button
-              onClick={ (e) => this.handleSendEvent(e) }
-              className='send'>
-              Send
-            </button>
-          </div>
-          <div className='rspec-display'>
-            <h1>Rspec</h1>
-            <ul className='chat-logs'>
-              { this.renderChatLog() }
-            </ul>
-            <div>
-              <textarea
-              onKeyPress={ (e) => this.handleTestBlockInputKeyPress(e) }
-              value={ this.state.currentTestBlock }
-              onChange={ (e) => this.updateCurrentTestBlock(e) }
-              type='text'
-              placeholder='Enter your code...'
-              className='test-code-input'></textarea>
-              <button
-                onClick={ (e) => this.handleTestSendEvent(e) }
-                className='send'>
-                Send
-              </button>
-            </div>
-            <input
-              onKeyPress={ (e) => this.handleChatInputKeyPress(e) }
-              value={ this.state.currentChatMessage }
-              onChange={ (e) => this.updateCurrentChatMessage(e) }
-              type='text'
-              placeholder='Enter your code...'
-              className='chat-input' />
-            <button
-              onClick={ (e) => this.handleSendEvent(e) }
-              className='send'>
-              Send
-            </button>
-          </div>
         </div>
       </div>
     );
   }
-  updateCurrentChatMessage(event) {
-    this.setState({
-      currentChatMessage: event.target.value
-    });
-  }
-
-  handleChatInputKeyPress(event) {
-    if(event.key === 'Enter') {
-      this.handleSendEvent(event);
-    }//end if
-  }
-
-  handleSendEvent(event) {
-    event.preventDefault();
-    this.chats.create(this.state.currentChatMessage);
-    this.setState({
-      currentChatMessage: ''
-    });
-  }
 }
 
 export default App;
+
