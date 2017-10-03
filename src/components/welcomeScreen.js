38ddeeeb76ff60beb0b29055e157b63683c458d7
Simon Brady <simonbrady@mac.com>
2017-10-03 16:20:39 -0400

    Changed to ABOUT

--- src/components/welcomeScreen.js
+++ src/components/welcomeScreen.js
@@ -6,7 +6,7 @@ class WelcomeScreen extends React.Component {
     return (
       <div>
         <section>
-          <button onClick={() => this.simpleDialog.show()}>About Me</button>
+          <button onClick={() => this.simpleDialog.show()}>ABOUT</button>
         </section>
         <SkyLight hideOnOverlayClicked ref={ref => this.simpleDialog = ref} title="Welcome to Rspectacle!">
           Using Rails` ActionCable and ReactJS, you can write and test Ruby Code between more than one user in real time!              
