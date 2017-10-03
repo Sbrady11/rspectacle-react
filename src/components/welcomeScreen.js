0119ff7374da4d2f7f10c99fe9c06eb8a6328637
Simon Brady <simonbrady@mac.com>
2017-10-03 15:37:10 -0400

    Added about us modal, and alerts that you clicked the button

---
+++ src/components/welcomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import SkyLight from 'react-skylight';
+
+class WelcomeScreen extends React.Component {
+  render() {
+    return (
+      <div>
+        <section>
+          <button onClick={() => this.simpleDialog.show()}>About Me</button>
+        </section>
+        <SkyLight hideOnOverlayClicked ref={ref => this.simpleDialog = ref} title="Welcome to Rspectacle!">
+          Using Rails` ActionCable and ReactJS, you can write and test Ruby Code between more than one user in real time!              
+
+          Grab a pal, write some rspec tests, and learn to love TDD<br></br>
+        </SkyLight>
+      </div>
+    );
+  }
+}
+
+
+export default WelcomeScreen;
